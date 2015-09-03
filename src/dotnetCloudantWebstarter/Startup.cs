ec4560950504b0a1886321d75ee8fda0ab8b02fc
Michael Peters <mtpeters@us.ibm.com>
2015-09-03 16:12:55 -0400

    read VCAP_SERVICES once and use dependency injection to obtain the credentials when needed

--- src/dotnetCloudantWebstarter/Startup.cs
+++ src/dotnetCloudantWebstarter/Startup.cs
@@ -4,31 +4,55 @@ using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.Runtime;
-using CloudantDotNet;
+using Newtonsoft.Json;
 
 public class Startup
 {
-	public IConfiguration Configuration { get; set; }
+    public IConfiguration Configuration { get; set; }
 
-	public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
-	{
-		var configBuilder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
-			.AddJsonFile("config.json")
-			.AddEnvironmentVariables();
-		Configuration = configBuilder.Build();
-	}
+    public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+    {
+        var configBuilder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+            .AddJsonFile("config.json");
+        Configuration = configBuilder.Build();
 
-	public void ConfigureServices(IServiceCollection services)
-	{
-		services.AddMvc();
-	}
+        string vcapServices = System.Environment.GetEnvironmentVariable("VCAP_SERVICES");
+        if (vcapServices != null)
+        {
+            dynamic json = JsonConvert.DeserializeObject(vcapServices);
+            if (json.cloudantNoSQLDB != null)
+            {
+                string host = json.cloudantNoSQLDB[0].credentials.host;
+                string username = json.cloudantNoSQLDB[0].credentials.username;
+                string password = json.cloudantNoSQLDB[0].credentials.password;
+                Configuration.Set("cloudantNoSQLDB:0:credentials:username", username);
+                Configuration.Set("cloudantNoSQLDB:0:credentials:password", password);
+                Configuration.Set("cloudantNoSQLDB:0:credentials:host", host);
+            }
+        }
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+
+        // works with VCAP_SERVICES JSON value added to config.json when running locally,
+        // and works with actual VCAP_SERVICES env var based on configuration set above when running in CF
+        services.Configure<creds>(Configuration.GetConfigurationSection("cloudantNoSQLDB:0:credentials"));
+    }
 
     public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
     {
         loggerFactory.AddConsole();
-		app.UseErrorPage();
-		app.UseStaticFiles();
-		app.UseMvcWithDefaultRoute();        
+        app.UseErrorPage();
+        app.UseStaticFiles();
+        app.UseMvcWithDefaultRoute();
     }
+}
 
+public class creds
+{
+    public string username { get; set; }
+    public string password { get; set; }
+    public string host { get; set; }
 }
